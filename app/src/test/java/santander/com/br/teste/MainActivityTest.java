fa9f552e6f6f9041e3ac44ae8c2bea881d2f5f86
Cledson <sophiaalvescamargo@hotmail.com>
2018-07-25 10:35:44 -0300

    init commit

---
+++ app/src/test/java/santander/com/br/teste/MainActivityTest.java
@@ -0,0 +1,13 @@
+package santander.com.br.teste;
+
+import org.junit.Test;
+
+/**
+ * Created by cledson.alves on 25/07/2018.
+ */
+
+public class MainActivityTest {
+    @Test
+    public void isActivity(){
+    }
+}
